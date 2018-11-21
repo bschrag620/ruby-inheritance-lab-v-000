6245699fcf61511e29698877fedb2fbf496ee018
Brad Schrag <brad.schrag@gmail.com>
2018-11-21 15:55:46 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,6 +3,11 @@ require_relative "./user.rb"
 class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+
+  def initialize
+    self.knowledge = KNOWLEDGE
+  end
+  
   def teach
     i = rand(self.knowledge.count - 1)
     puts "i: #{i}"
